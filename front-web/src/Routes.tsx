eb6d69c06c7eb7260d9961ac0f3aaebb4227e521
Joao Henrique <joao.gh61@gmail.com>
2021-01-09 11:05:52 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Routes.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './home';
+import Navbar from './navbar';
+import Orders from './orders';
+function Routes() {
+    return (
+        <BrowserRouter>
+        <Navbar />
+            <Switch>
+                <Route path="/orders">
+                    <Orders />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
---
+++ front-web/src/orders/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+function Orders() {
+    return (
+        <div className="orders-continer">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders;
